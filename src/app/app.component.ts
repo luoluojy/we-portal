9899fe5f44e2bff8630b15b5e2ad68432167a14b
程立龙 <luoluojy@hotmail.com>
2019-05-28 19:52:26 +0800

    升级最初代码到angular7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,45 @@
-import { Component,OnInit,ElementRef,ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
-import {Title} from '@angular/platform-browser';
-import {Router,NavigationEnd,ActivatedRoute} from '@angular/router';
-
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-
-import '../assets/css/styles.css';
-import '../assets/libs/js/bootstrap.min.js';
-import '../assets/libs/js/jquery.min.js';
-import '../assets/libs/css/bootstrap.min.css';
-import '../assets/libs/js/jQueryRotate.js';
-declare let $:any;
+declare let $: any;
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
- @ViewChild('course')
- courseLi:ElementRef;
-  constructor(private titleService:Title,private activatedRoute:ActivatedRoute,private router:Router){}
-  ngOnInit(){
+  constructor(
+    private titleService: Title,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+  @ViewChild('course')
+  courseLi: ElementRef;
+  degree: number;
+  ngOnInit() {
     this.router.events
-      .filter(event=>event instanceof NavigationEnd)
-      .map(()=>this.activatedRoute)
-      .map(route=>{
-        while(route.firstChild) route=route.firstChild;
-        return route
-      })
-      .filter(route=>route.outlet==='primary')
-      .mergeMap(route=>route.data)
-      .subscribe((event)=>this.titleService.setTitle(event['title']));
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map(route => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter(route => route.outlet === 'primary'),
+        mergeMap(route => route.data)
+      )
+      .subscribe(event => this.titleService.setTitle(event.title));
   }
-    degree:number;
-  toggle(){
-        if(this.degree==180)
-          this.degree=0;
-        else 
-          this.degree=180;
-        $(this.courseLi.nativeElement.querySelector('span')).rotate(this.degree);
-      
+  toggle() {
+    if (this.degree === 180) {
+      this.degree = 0;
+    } else {
+      this.degree = 180;
     }
+    $(this.courseLi.nativeElement.querySelector('span')).rotate(this.degree);
+  }
 }
