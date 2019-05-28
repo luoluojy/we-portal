48c5d1d2200561ca59bc621ac452afa9e578d4c7
程立龙 <luoluojy@hotmail.com>
2019-05-28 20:14:46 +0800

    移除hash路由模式为h5模式

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-
-import {RouterModule,Routes} from '@angular/router';
-import {AppRoutingModule} from './app-routing.module';
-import {ProgressRoutingModule} from './progress/progress-routing.module';
-import { AppComponent } from './app.component';
-import {ProgressModule} from './progress/progress.module';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProgressModule } from './progress/progress.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     ProgressModule,
-    AppRoutingModule,BrowserAnimationsModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [ AppComponent ],
-  providers: [
-    {provide: LocationStrategy, useClass: HashLocationStrategy}
-  ]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule { }
+export class AppModule {}
